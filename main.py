ad2bf7c76ee81a5543575991d8478ef05bf084fb
Najjar-abdelbaki <117846854+Najjar-abdelbaki@users.noreply.github.com>
2023-07-20 14:57:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,38 +21,27 @@ article = soup.find("h1", class_="h1").text
 
 
 
-print("Welcome to Traker price.\n \
-We find the best price deals and email them to you.")
 
-first_name = input("What is your first name? ").title()
-last_name = input("What is your last name? ").title()
 
-email1 = "email1"
-email2 = "email2"
-while email1 != email2:
-    email1 = input("What is your email? ")
-    if email1.lower() == "quit" \
-            or email1.lower() == "exit":
-        exit()
-    email2 = input("Please verify your email : ")
-    if email2.lower() == "quit" \
-            or email2.lower() == "exit":
-        exit()
-
-print("OK. You're in the club!")
-
-sheety.post_new_row(first_name, last_name, email1)
 data = sheety.get_customer_emails()
 emails = [row["email"] for row in data]
 names = [row["firstName"] for row in data]
-
-
-
-if price_int < 53 :
-    message = f"Subject:Pharma Price Alert!\n\n{article} is now {price_int}DT"
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=password)
-        for email in emails:
-
+prices = [row["bedget"] for row in data]
+for p, email, name in zip(prices, emails, names):
+    if price_int <= float(p) :
+        message = f"Subject:Pharma Price Alert!\n\nhi! {name}\n{article} is now {price_int}DT\n{URL}"
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=password)
             connection.sendmail(from_addr=MY_EMAIL, to_addrs=email, msg=message)
+
+
+
+# if price_int < 52 :
+#     message = f"Subject:Pharma Price Alert!\n\n{article} is now {price_int}DT\n{URL}"
+#     with smtplib.SMTP("smtp.gmail.com") as connection:
+#         connection.starttls()
+#         connection.login(user=MY_EMAIL, password=password)
+#         for email in emails:
+#
+#             connection.sendmail(from_addr=MY_EMAIL, to_addrs=email, msg=message)
